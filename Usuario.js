cca70ddcd3b483e5ec9a22d19233c5fd8f76fec8
tinchobecchio <tincho.becchio@gmail.com>
2026-08-20 19:23:55 -0300

    creacion clase Usuario

---
+++ Usuario.js
@@ -0,0 +1,12 @@
+export class Usuario {
+    constructor(nombre, email) {
+        this.nombre = nombre
+        this.email = email
+        this.fechaRegistro = new Date()
+    }
+    mostrarPerfil(){
+        return `Nombre: ${this.nombre} Email: ${this.email}`
+    }
+}
+
+export default Usuario
