d23566bb6dadf2ca07ab83ea612ebfa8f95096b0
tinchobecchio <tincho.becchio@gmail.com>
2026-08-20 20:30:06 -0300

    Parte 4: Iniciamos RepositorioPublicaciones

---
+++ RepositorioPublicaciones.js
@@ -0,0 +1,24 @@
+class RepositorioPublicaciones {
+    constructor(){
+
+    }
+    publicaciones = [] //arreglo de objetos Publicacion
+
+    agregar(publicacion) { //publicacion: objeto Publicacion
+        this.publicaciones.push(publicacion)
+    }
+
+    buscarPorUsuario(nombre) {
+        return this.publicaciones.filter(p => p.autor.nombre === nombre)
+    }
+
+    filtrarActivas() {
+        return this.publicaciones.filter(p => p.estaActiva())
+    }
+
+    cantidadTotal() {
+        return this.publicaciones.length
+    }
+}
+
+export default RepositorioPublicaciones
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import Publicacion from "./Publicacion.js";
+import RepositorioPublicaciones from "./RepositorioPublicaciones.js";
 import Usuario from "./Usuario.js";
 
 let usuarios = [
-    new Usuario("Santiago","santi@gmail.com"),
+    new Usuario("Santiago", "santi@gmail.com"),
     new Usuario("Martin", "tincho@gmail.com"),
     new Usuario("Fabricio", "fabri@gmail.com"),
 ]
@@ -23,9 +24,32 @@ const publicacionesActivas = publicaciones.filter(publicacion => publicacion.est
 const primeraPublicacion = publicaciones.find(publicacion => publicacion.autor.nombre === "Santiago")
 
 console.log("Publicaciones activas:", publicacionesActivas);
-console.log("Primera publicacion por nombre:",primeraPublicacion);
+console.log("Primera publicacion por nombre:", primeraPublicacion);
 
 // Verificacion
 usuarios[2].email = "fabricio@gmail.com"
 const publicacionesFabri = publicaciones.filter(publicacion => publicacion.autor.nombre === "Fabricio")
 console.log(publicacionesFabri);
+
+
+// Parte 4
+const repositorio = new RepositorioPublicaciones()
+publicaciones.forEach(publi => repositorio.agregar(publi))
+console.log("-----------------------");
+
+repositorio.publicaciones.forEach(publi => console.log(publi))
+
+console.log("Publicaciones de Santiago: ", repositorio.buscarPorUsuario("Santiago"))
+console.log("Publicaciones de Martin: ", repositorio.buscarPorUsuario("Martin"))
+console.log("Publicaciones de Fabricio: ", repositorio.buscarPorUsuario("Fabricio"))
+
+// parte 4 desafio 1
+console.log("------------------------");
+console.log("------------------------");
+publicaciones[1].activa = false
+publicaciones[3].activa = false
+console.log(repositorio.filtrarActivas())
+
+// parte 4 desafio 2
+console.log("------------------------");
+console.log("Cantidad de publicaciones: ", repositorio.cantidadTotal());
