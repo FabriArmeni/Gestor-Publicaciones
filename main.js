04fd73ea140a19aa160cbb6ae09c530104aafb30
tinchobecchio <tincho.becchio@gmail.com>
2026-08-20 19:52:22 -0300

    Parte 3: Array de publicaciones y Usuarios

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
 import Publicacion from "./Publicacion.js";
+import Usuario from "./Usuario.js";
+
+let usuarios = [
+    new Usuario("Santiago","santi@gmail.com"),
+    new Usuario("Martin", "tincho@gmail.com"),
+    new Usuario("Fabricio", "fabri@gmail.com"),
+]
 
 let publicaciones = [
-    new Publicacion("apuntes matematica", "Primeros ejercicios", "santi"),
-    new Publicacion("apuntes guitarra", "Acordes basicos", "paco"),
-    new Publicacion("clases karate", "Defensa personal", "martin"),
-    new Publicacion("resumen libro", "El señor de los anillos", "fabri")
+    new Publicacion("apuntes matematica", "Primeros ejercicios", usuarios[0]),
+    new Publicacion("apuntes guitarra", "Acordes basicos", usuarios[0]),
+    new Publicacion("clases karate", "Defensa personal", usuarios[1]),
+    new Publicacion("resumen libro", "El señor de los anillos", usuarios[2]),
+    new Publicacion("examenes previos", "Apuntes para estudiar los examenes anteriores", usuarios[2])
 ];
 
-publicaciones[1].activa = false;
+publicaciones.forEach(publicacion => {
+    console.log("Resumen", publicacion.mostrarResumen());
+})
 
-let quinta = new Publicacion("vender cursos", "Ser tu propio jefe", "nacho")
-publicaciones.push(quinta)
+const publicacionesActivas = publicaciones.filter(publicacion => publicacion.estaActiva())
+const primeraPublicacion = publicaciones.find(publicacion => publicacion.autor.nombre === "Santiago")
 
-for (let i = 0; i < publicaciones.length; i++){
-    console.log(publicaciones[i].mostrarResumen())
-    console.log(publicaciones[i].estaActiva())
-}
-console.log("---------------------------------------------")
-console.log("---------------------------------------------")
-let contador = 0;
-for (let i = 0; i < publicaciones.length; i++){
-    if (publicaciones[i].activa === true){
-        contador++;
-        console.log(publicaciones[i].mostrarResumen())
-        console.log(publicaciones[i].estaActiva())
-    }
-}
-console.log(`Publicaciones activas = ${contador}`)
+console.log("Publicaciones activas:", publicacionesActivas);
+console.log("Primera publicacion por nombre:",primeraPublicacion);
+
+// Verificacion
+usuarios[2].email = "fabricio@gmail.com"
+const publicacionesFabri = publicaciones.filter(publicacion => publicacion.autor.nombre === "Fabricio")
+console.log(publicacionesFabri);
