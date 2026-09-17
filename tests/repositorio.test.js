c128ca99e04e3129cf17d83124533e8478094e00
tinchobecchio <tincho.becchio@gmail.com>
2026-09-17 15:53:40 -0300

    Parte 6 · Pruebas sobre el repositorio

--- tests/repositorio.test.js
+++ tests/repositorio.test.js
@@ -1,3 +1,27 @@
-describe("repositorio", () => {
-    test("prueba", () => {})
-})
+// import { RepositorioPublicaciones } from "../src/RepositorioPublicaciones.js";
+// import { Publicacion } from "../src/Publicacion.js";
+
+import RepositorioPublicaciones from "../public/src/RepositorioPublicaciones.js";
+import Publicacion from "../public/src/Publicacion.js";
+
+describe("RepositorioPublicaciones", () => {
+    test("buscarPorEtiqueta devuelve coincidencias activas", () => {
+        const repositorio = new RepositorioPublicaciones();
+        const publicacion = new Publicacion("Ana", "Apuntes de Redes", "...");
+        publicacion.agregarEtiqueta("redes");
+        repositorio.agregar(publicacion);
+        expect(repositorio.buscarPorEtiqueta("redes")).toEqual([publicacion]);
+    });
+    test("una publicación dada de baja queda excluida", () => {
+        const repositorio = new RepositorioPublicaciones();
+        const publicacion = new Publicacion("Ana", "Apuntes de Redes", "...");
+        publicacion.agregarEtiqueta("redes");
+        publicacion.darDeBaja();
+        repositorio.agregar(publicacion);
+        expect(repositorio.buscarPorEtiqueta("redes")).toEqual([]);
+    });
+    test("una etiqueta inexistente devuelve un arreglo vacío", () => {
+        const repositorio = new RepositorioPublicaciones();
+        expect(repositorio.buscarPorEtiqueta("inexistente")).toEqual([]);
+    });
+});
