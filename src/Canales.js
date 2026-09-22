065286f942cdaac3be5d2b370191ac4b99adc941
tinchobecchio <tincho.becchio@gmail.com>
2026-09-22 20:46:40 -0300

    Parte 4 · Canales de notificación

---
+++ src/Canales.js
@@ -0,0 +1,10 @@
+export class NotificadorWeb {
+    notificar(mensaje) {
+        return `Notificación web: ${mensaje}`;
+    }
+}
+export class NotificadorEmail {
+    notificar(mensaje) {
+        return `Email enviado: ${mensaje}`;
+    }
+}
